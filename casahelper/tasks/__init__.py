50141576567ebcb1611b88c58634ad8bad3c8247
Patrick Sheehan <psheehan287@gmail.com>
2021-01-02 08:43:08 -0500

    Fixed a few issues in __init__.py's.

--- casahelper/tasks/__init__.py
+++ casahelper/tasks/__init__.py
@@ -2,7 +2,6 @@ from .reset_ms import reset_ms
 from .export_continuum import export_continuum
 from .export_lines import export_lines
 from .flag_lines import flag_lines
-from .image_and_self_calibrate import image_and_self_calibrate
 from .image_continuum import image_continuum
 from .image_lines import image_lines
 from .self_calibrate import self_calibrate
