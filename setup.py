5aab51c9b824be7a7363c8ab9da11490f568bd8e
Patrick Sheehan <psheehan287@gmail.com>
2021-01-02 08:42:02 -0500

    Added functionality to generate basic scripts.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="casarecipes", 
+    name="casahelper", 
     version="0.0.1",
     author="Patrick Sheehan",
     author_email="psheehan287@gmail.com",
@@ -13,11 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/psheehan/casarecipes",
     packages=setuptools.find_packages(),
+    scripts=["bin/casahelper-tool"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL3 License",
         "Operating System :: OS Independent",
     ],
     install_requires=['numpy'],
-    python_requires='==3.6',
+    python_requires='==3.6.*',
 )
