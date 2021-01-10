51678ad263749010b0424e7070b4c6ab909afa80
Patrick Sheehan <psheehan287@gmail.com>
2021-01-10 16:43:16 -0600

    plotcal depends on casaplotms

--- casahelper/utils/plotcal.py
+++ casahelper/utils/plotcal.py
@@ -1,4 +1,4 @@
-import casatasks
+from casaplotms import plotms
 
 # Before getting started, define a procedure which will plot the calibration
 # in a way I'm happy with so that I don't have to type it over and over again.
@@ -12,7 +12,7 @@ def plotcal(caltable=None, xaxis=None, yaxis=None, iteraxis='antenna', \
     elif yaxis == 'amp':
         plotrange = []
 
-    casatasks.plotms(vis=caltable, xaxis=xaxis, yaxis=yaxis, iteraxis=iteraxis,\
+    plotms(vis=caltable, xaxis=xaxis, yaxis=yaxis, iteraxis=iteraxis,\
             coloraxis=coloraxis, spw='', gridrows=7, gridcols=7, showgui=False,\
             plotfile=filename, plotrange=plotrange, width=3000, height=3000,\
             dpi=300, overwrite=True)
