d883664c62f6cec0591ac9285d020b0ecf5f4769
Patrick Sheehan <psheehan287@gmail.com>
2021-01-09 18:29:48 -0600

    Base image_advice on baseline percentiles instead of max baseline.

--- casahelper/utils/image_advice.py
+++ casahelper/utils/image_advice.py
@@ -1,10 +1,13 @@
 # Tool to estimate image parameters.
 
 from .Track import Track
+from .get_spwsforband import get_spwsforband
+from .get_bands import get_bands
 import casatools
+import numpy
 
 def image_advice(track=None, fieldofview="30arcsec", mask="auto-multithresh", \
-        array="ALMA-auto"):
+        array="ALMA-auto", usepercentile=80):
     # Check that the value provided for track is appropriate.
 
     if type(track) == Track:
@@ -16,7 +19,8 @@ def image_advice(track=None, fieldofview="30arcsec", mask="auto-multithresh", \
 
     # Create an instance of the imager tool.
 
-    im = casatools.imager()
+    ms = casatools.ms()
+    msmd = casatools.msmetadata()
 
     # Create a dictionary to put the advice.
 
@@ -25,13 +29,41 @@ def image_advice(track=None, fieldofview="30arcsec", mask="auto-multithresh", \
     # Get the imager advice on cells and npix.
 
     if filename != None:
-        im.open(filename)
-        initial_advice = im.advise(fieldofview=fieldofview)
-        im.close()
+        # Get the baselines from the dataset.
 
-        advice["imsize"] = initial_advice[1]*3
-        advice["cell"] = str(initial_advice[2]["value"]/3) + \
-                initial_advice[2]["unit"]
+        ms.open(filename)
+        ms.selectinit(0)
+        baselines = ms.getdata(items=['uvdist'])['uvdist']
+        ms.close()
+
+        # Get the frequencies and average to get the mean frequency.
+
+        spws = get_spwsforband(filename, band=get_bands(filename)[0])
+
+        msmd.open(filename)
+        freqs = numpy.concatenate([msmd.chanfreqs(spw) for spw in spws])
+        widths = numpy.concatenate([msmd.chanwidths(spw) for spw in spws])
+        msmd.close()
+
+        mean_freq = (freqs * widths).sum() / widths.sum()
+
+        # Convert baselines from m to lambda.
+
+        baselines *= mean_freq / 3e8
+
+        # Estimate the beam size from the baseline at the 80th percentile.
+
+        l80 = numpy.percentile(baselines, 80)
+
+        beam = 0.574 / l80 / 4.8481e-6
+
+        # Use the beam size to calculate the appropriate cell size.
+
+        cell = beam / 2
+
+        advice["cell"] = str(cell/5)+"arcsec"
+        advice["imsize"] = max(64, int(float(fieldofview.split("a")[0])/ \
+                (cell) / 10 + 1)*10)*5
 
     # Add the mask.
 
@@ -47,15 +79,16 @@ def image_advice(track=None, fieldofview="30arcsec", mask="auto-multithresh", \
                 raise RuntimeError("For auto selecting of auto-multithresh "
                         "parameters, a filename must be provided.")
             else:
-                cell = initial_advice[2]["value"]
+                # Use the 75th percentile baseline to determine the array.
 
-                if cell > 2:
+                b75 = numpy.percentile(baselines*3e8/mean_freq, 75)
+
+                if b75 < 45:
                     array = "ALMA-ACA"
-                elif cell > 0.2:
+                elif b75 < 300:
                     array = "ALMA-SB"
                 else:
                     array = "ALMA-LB"
-
 
         # Now, get the auto-masking parameters.
 
